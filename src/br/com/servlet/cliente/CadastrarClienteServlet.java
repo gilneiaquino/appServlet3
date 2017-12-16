3fce05fe6186103373c52e06dbe9984348b7b522
gilnei <gilnei.aquino@gmail.com>
2017-12-15 22:55:41 -0200

    estudo servlet e filter

--- src/br/com/servlet/cliente/CadastrarClienteServlet.java
+++ src/br/com/servlet/cliente/CadastrarClienteServlet.java
@@ -2,32 +2,55 @@ package br.com.servlet.cliente;
 
 import java.io.IOException;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = { "/cadastrarCliente",
+		"/formCliente" }, name = "primeiroServlet", description = "vai ser ele o primeiro", 
+		initParams = {
+				@WebInitParam(name = "cidade", value = "brasilia"), 
+				@WebInitParam(name = "idadeCidade", value = "50")
 
+}, loadOnStartup = 1)
+public class CadastrarClienteServlet extends HttpServlet {
 
-public class CadastrarClienteServlet extends HttpServlet{
-	
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		//System.out.println("iniciando o cadastro de cliente");
+		// System.out.println("iniciando o cadastro de cliente");
 
 	}
-	
+
 	@Override
-	protected void doPost (HttpServletRequest request,
-	        HttpServletResponse response)
-	        throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String nomeCliente = request.getParameter("nome");
 		Integer idadeCliente = Integer.parseInt(request.getParameter("idade"));
-		
-		System.out.println("cadastrou o clinte:"+nomeCliente + "Com a idade: "+ idadeCliente);
+
+		String minhaCidade = getInitParameter("cidade");
+
+		String contextCidade = getServletContext().getInitParameter("idadeCidade");
+		String configCidade = getServletConfig().getInitParameter("idadeCidade");
+
+		System.out.println("cadastrou o clinte:" + nomeCliente + "Com a idade: " + idadeCliente);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("cadastrou o cliente");
+
+		RequestDispatcher rd = req.getRequestDispatcher("/clienteArmazenadoTeste");
+
+		rd.forward(req, resp);
 	}
 
 }
---
+++ src/br/com/servlet/login/FiltroLogin.java
@@ -0,0 +1,38 @@
+package br.com.servlet.login;
+
+import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(dispatcherTypes= {DispatcherType.REQUEST,DispatcherType.FORWARD},
+filterName="filtro1", asyncSupported=true)
+public class FiltroLogin implements Filter{
+	
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)throws ServletException,IOException {
+    	chain.doFilter(req, resp);
+    	
+    }
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
+
+}
